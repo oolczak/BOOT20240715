8c656308c978b2fce099b9838ea6ae6c3bf180ef
Javier <jmartin@grupoloyal.com>
2024-07-19 09:30:49 +0200

    quoted

--- SQL/ejemplos.sql
+++ SQL/ejemplos.sql
@@ -5,6 +5,10 @@ DEsc jobs; -- Esto es un ejempo
 SELECT DEPARTMENT_ID, DEPARTMENT_NAME 
 FROM DEPARTMENTS;
 /
-SELECT last_name, LAST_NAME || ', ' || FIRST_NAME, salary*12 --, SALARY
+SELECT last_name, LAST_NAME || q'( algo's )' || FIRST_NAME as "Nombre Completo", salary*12 "Salario Anual" --, SALARY
 FROM   employees;
 /
+SELECT last_name, job_id, salary, commission_pct, salary * NVL(commission_pct,0)
+FROM   employees;
+/
+
