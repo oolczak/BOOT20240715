6691634601211ef89a626da6955ccccfd0e88953
Javier <jmartin@grupoloyal.com>
2024-07-29 09:20:57 +0200

    pl2

--- PLSQL/ejemplos.pl.sql
+++ PLSQL/ejemplos.pl.sql
@@ -1,6 +1,8 @@
 DECLARE
     v_nombre VARCHAR2(100); -- not null; -- default 'Mundo';
+    v_LAST_NAME EMPLOYEES.FIRST_NAME%TYPE;
     v_cuantos NUMBER;
+    total NUMBER;
 BEGIN
     --DBMS_OUTPUT.PUT_LINE('Hola ' || v_nombre);
     --v_nombre := 'tu';
@@ -9,10 +11,11 @@ BEGIN
     from EMPLOYEES 
     where EMPLOYEE_ID = 1203;
     DBMS_OUTPUT.PUT_LINE('Cuantos: ' || v_cuantos);
-    select FIRST_NAME
-    into v_nombre
+    v_cuantos := 1.2;
+    select FIRST_NAME, LAST_NAME || v_LAST_NAME, SALARY * v_cuantos
+    into v_nombre, v_LAST_NAME, total
     from EMPLOYEES 
     where EMPLOYEE_ID = 203;
-    DBMS_OUTPUT.PUT_LINE('Hola ' || nvl(v_nombre, '(anonimo)'));
+    DBMS_OUTPUT.PUT_LINE('Hola ' || nvl(v_nombre, '(anonimo)') || ' tu salario es ' || total);
 --EXCEPTION
 END;
