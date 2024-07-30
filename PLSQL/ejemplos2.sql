e08e7fb3c1d9117c5441758b6aa88b40dca3507f
Javier <jmartin@grupoloyal.com>
2024-07-30 08:12:21 +0200

    lab 06

--- PLSQL/ejemplos2.sql
+++ PLSQL/ejemplos2.sql
@@ -1,6 +1,7 @@
 DECLARE
     v_cuantos NUMBER;
     total NUMBER;
+
 BEGIN
     --DBMS_OUTPUT.PUT_LINE('Hola ' || v_nombre);
     --v_nombre := 'tu';
@@ -22,9 +23,37 @@ BEGIN
             DBMS_OUTPUT.PUT_LINE('Hola ' || nvl(v_nombre, '(anonimo)') || ' tu salario es ' || total);
         END;
     elsif v_cuantos = 0 THEN
-        DBMS_OUTPUT.PUT_LINE('No hay ninguno');
+        <<exterior>>
+        for x in 1..3 loop
+            for i in reverse 1..10 by 2 loop
+                DBMS_OUTPUT.PUT_LINE('No hay ninguno ' || i);
+                EXIT when i in (6,5);
+                -- EXIT exterior when x > 1;
+                -- if i in (6,5) then goto sigo; end if;
+            end loop;
+            DBMS_OUTPUT.PUT_LINE('Terminada la vuelta ' || x);
+        end loop;
     else 
         DBMS_OUTPUT.PUT_LINE('Hay demasiados');
     end if;
+        DBMS_OUTPUT.PUT_LINE('sale bucle');
+    <<sigo>>
+        DBMS_OUTPUT.PUT_LINE('Terminado');
+   
 --EXCEPTION
 END;
+
+DECLARE 
+    v_max_deptno  NUMBER; 
+    v_dept_name departments.department_name%TYPE:= 'Education'; 
+    v_dept_id NUMBER;   
+BEGIN 
+    SELECT MAX(department_id)  INTO v_max_deptno  FROM departments;
+    DBMS_OUTPUT.PUT_LINE('The maximum department_id is : ' || v_max_deptno); 
+    v_dept_id := 10 + v_max_deptno; 
+    INSERT INTO departments (department_id, department_name, 
+    location_id)  
+    VALUES (v_dept_id, v_dept_name, NULL);
+    DBMS_OUTPUT.PUT_LINE (' SQL%ROWCOUNT gives ' || SQL%ROWCOUNT);
+END;
+-- ROLLBACK;
