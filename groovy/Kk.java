f4898d7d0dba4664a525314595c48cd958464118
Javier <jmartin@grupoloyal.com>
2024-08-08 08:07:49 +0200

    soluciones

--- groovy/Kk.java
+++ groovy/Kk.java
@@ -1,10 +1,18 @@
-import java.util.regex.Matcher;
 
-class Kk {
+class Kk implements Comparable {
     void cotilla() {
-        var cad = "";
-        cad.toLowerCase()
-        (new Matcher()).groupCount()
+        // var cad = "";
+        // cad.replace();
+        // // java.util.regex.Matcher()
+        // java.text.Normalizer.normalize(cadena.toLowerCase(), java.text.Normalizer.Form.NFD)
+        // .replace(~/[.,;:#¿?¡!(){}]/, '')
+        // .replace(' ', '');
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
     }
 }
 
